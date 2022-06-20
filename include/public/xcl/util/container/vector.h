b9c1cf3a64a9f9e800020900c354dd4e3e46c756
BigDev <1404974345@qq.com>
2022-06-20 16:49:16 +0800

    delete xcl

--- include/public/xcl/util/container/vector.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by 徐琰 on 2021/12/10.
-//
-
-#ifndef SCL_VECTOR_H
-#define SCL_VECTOR_H
-
-#endif// SCL_VECTOR_H
--- src/public/xcl/util/container/string.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by 徐琰 on 2021/10/15.
-//
-
-#include <xcl/util/container/string.h>
