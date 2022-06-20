f6dde9f3601933c72e088e6582c42c7318534c33
BigDev <1404974345@qq.com>
2022-06-20 16:54:21 +0800

    new xcl

---
+++ cmake-build-debug/CMakeFiles/xcl2.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/ClionProjects/xcl2/src/concurrent/util/CThreadLocal.c" "D:/ClionProjects/xcl2/cmake-build-debug/CMakeFiles/xcl2.dir/src/concurrent/util/CThreadLocal.c.obj"
+  "D:/ClionProjects/xcl2/src/ini/IniParse.c" "D:/ClionProjects/xcl2/cmake-build-debug/CMakeFiles/xcl2.dir/src/ini/IniParse.c.obj"
+  "D:/ClionProjects/xcl2/src/ini/cfg.c" "D:/ClionProjects/xcl2/cmake-build-debug/CMakeFiles/xcl2.dir/src/ini/cfg.c.obj"
+  "D:/ClionProjects/xcl2/src/util/CBuffer.c" "D:/ClionProjects/xcl2/cmake-build-debug/CMakeFiles/xcl2.dir/src/util/CBuffer.c.obj"
+  "D:/ClionProjects/xcl2/src/util/CList.c" "D:/ClionProjects/xcl2/cmake-build-debug/CMakeFiles/xcl2.dir/src/util/CList.c.obj"
+  "D:/ClionProjects/xcl2/src/util/CString.c" "D:/ClionProjects/xcl2/cmake-build-debug/CMakeFiles/xcl2.dir/src/util/CString.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "xcl2_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "C:/Program Files (x86)/googletest-distribution/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/ClionProjects/xcl2/src/pool/pool.cc" "D:/ClionProjects/xcl2/cmake-build-debug/CMakeFiles/xcl2.dir/src/pool/pool.cc.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "xcl2_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "C:/Program Files (x86)/googletest-distribution/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
