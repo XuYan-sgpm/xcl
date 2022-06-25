19316ad8ba87a69585c1b0fc963404a5ea0c1701
BigDev <1404974345@qq.com>
2022-06-25 12:17:11 +0800

    0625

--- test/TestLock.cc
+++ test/TestLock.cc
@@ -5,18 +5,32 @@
 #include <gtest/gtest.h>
 #include "concurrent/Lock.h"
 #include <iostream>
+#include <unistd.h>
 using namespace std;
 
+static void *__testLock(void *arg) {
+  auto *lock = (xcl::TimedLock *)arg;
+  for (;;) {
+    if (!lock->tryLock(2000)) {
+      cout << "acquire lock failed" << endl;
+    } else {
+      break;
+    }
+  }
+  cout << "lock successfully" << endl;
+  sleep(3);
+  lock->unlock();
+  cout << "unlock successfully" << endl;
+  return nullptr;
+}
+
 TEST(TestLock, func1) {
-  xcl::Lock *lock = xcl::Lock::NewLock();
-  lock->lock();
-  lock->unlock();
-  cout << "step1" << endl;
-  lock->lock();
-  lock->unlock();
-  cout << "step2" << endl;
-  lock->lock();
-  cout << "step3" << endl;
-  lock->lock();
-  cout << "step4" << endl;
+  xcl::Lock *lock = xcl::TimedLock::NewLock();
+  pthread_t threads[4];
+  for (int i = 0; i < 4; i++) {
+    ASSERT_EQ(pthread_create(&threads[i], nullptr, __testLock, lock), 0);
+  }
+  for (int i = 0; i < 4; i++) {
+    pthread_join(threads[i], nullptr);
+  }
 }
