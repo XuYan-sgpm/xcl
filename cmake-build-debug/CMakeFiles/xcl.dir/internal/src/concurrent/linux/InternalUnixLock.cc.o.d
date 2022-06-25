ce21a0da5bf6c8e597fc298c110f92cf13d6f052
徐琰 <xuyan@xuyandeMacBook-Pro.local>
2022-06-25 18:05:08 +0800

    0625

--- cmake-build-debug/CMakeFiles/xcl.dir/internal/src/concurrent/linux/InternalUnixLock.cc.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/xcl.dir/internal/src/concurrent/linux/InternalUnixLock.cc.o: \
-  /Users/xuyan/repositories/xcl/internal/src/concurrent/linux/InternalUnixLock.cc \
-  /Users/xuyan/repositories/xcl/include/lang/platform.h
--- cmake-build-debug/CMakeFiles/xcl.dir/internal/src/concurrent/windows/InternalWinLock.cc.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/xcl.dir/internal/src/concurrent/windows/InternalWinLock.cc.o: \
-  /Users/xuyan/repositories/xcl/internal/src/concurrent/windows/InternalWinLock.cc \
-  /Users/xuyan/repositories/xcl/include/lang/platform.h
