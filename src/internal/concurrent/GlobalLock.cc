06d3c58533f35ecb33928477f11b1a7cb12384f4
徐琰 <xuyan@xuyandeMacBook-Pro.local>
2022-07-24 10:55:14 +0800

    update xcl

--- src/internal/concurrent/GlobalLock.cc
+++ src/internal/concurrent/GlobalLock.cc
@@ -6,14 +6,29 @@
 #include "xcl/concurrent/GlobalLock.h"
 #include "xcl/lang/XclDef.h"
 #include "xcl/concurrent/CMutex.h"
+#include "xcl/pool/CPool.h"
 
-static CMutex* __Global_mutex = nullptr;
+static CMutex* __XCL_globalMutex = nullptr;
+
+extern "C" {
+CMutex* __Mutex_newByPool(CPool* pool);
+
+bool __acquireGlobalLock()
+{
+    return Mutex_lock(__XCL_globalMutex);
+}
+
+void __releaseGlobalLock()
+{
+    Mutex_unlock(__XCL_globalMutex);
+}
+}
 
 #if CLANG || GNUC
-static __attribute__((constructor)) void __initializeGlobalMutex()
+static __attribute__((constructor)) void __initXclGlobalMutex()
 {
-    __Global_mutex = Mutex_new();
-    assert(__Global_mutex);
+    __XCL_globalMutex = __Mutex_newByPool(NULL);
+    assert(__XCL_globalMutex);
 }
 #elif defined(ENABLE_CXX)
 namespace xcl
@@ -28,8 +43,8 @@ namespace xcl
 
         xcl::GlobalMutexInitializer::GlobalMutexInitializer()
         {
-            __Global_mutex = Mutex_new();
-            assert(__Global_mutex);
+            __XCL_globalMutex = __Mutex_newByPool(NULL);
+            assert(__XCL_globalMutex);
         }
     }// namespace
     static GlobalMutexInitializer __globalMutexInitializer;
@@ -37,13 +52,3 @@ namespace xcl
 #else
 #error "invalid environment"
 #endif
-
-extern "C" bool __acquireGlobalLock()
-{
-    return Mutex_lock(__Global_mutex);
-}
-
-extern "C" void __releaseGlobalLock()
-{
-    Mutex_unlock(__Global_mutex);
-}
