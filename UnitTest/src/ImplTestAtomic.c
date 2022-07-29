bf677597e2e50e1a8a91633479b6c0fd4a7d8d91
BigDev <1404974345@qq.com>
2022-07-29 18:17:50 +0800

    update xcl

--- UnitTest/src/ImplTestAtomic.c
+++ UnitTest/src/ImplTestAtomic.c
@@ -14,11 +14,11 @@ void
 __testAtomic()
 {
     ATOMIC(TaggedPtr) x;
-    //    TaggedPtr ptr1 = { 0,98};
-    //    ATOMIC_STORE(&x, ptr1, memory_order_relaxed);
-    //    TaggedPtr data;
-    //    ATOMIC_LOAD(&x, data, memory_order_relaxed);
-    //    printf("%lld\n", data.tag);
+    TaggedPtr ptr1 = {0, 98};
+    ATOMIC_STORE(&x, ptr1, memory_order_relaxed);
+    TaggedPtr data;
+    ATOMIC_LOAD(&x, data, memory_order_relaxed);
+    printf("%lld\n", data.tag);
     TaggedPtr ptr = {0, 98};
     CAS(&x, ptr, ptr, memory_order_acq_rel);
 }
