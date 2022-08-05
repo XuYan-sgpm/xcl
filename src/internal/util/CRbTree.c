8f876a08c7957f2a048aec202d970ba0d1873f30
BigDev <1404974345@qq.com>
2022-08-05 18:12:13 +0800

    update xcl

--- src/internal/util/CRbTree.c
+++ src/internal/util/CRbTree.c
@@ -1,19 +1,34 @@
 #include "xcl/util/CRbTree.h"
 #include <xcl/lang/XclErr.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 CRbNode*
-__RbTree_allocNode()
+__RbTree_allocNode(int32_t size)
 {
 #ifdef _MSC_VER
-    void* p = _aligned_malloc(sizeof(CRbNode), sizeof(long));
+    void* p = _aligned_malloc(sizeof(CRbNode) + size, sizeof(long));
+    if (!p)
+    {
+        Err_set(XCL_MEMORY_ERR);
+    }
 #else
     void* p = 0;
-    int ret = posix_memalign(&p, sizeof(CRbNode), sizeof(long));
+    int ret = posix_memalign(&p, sizeof(CRbNode) + size, sizeof(long));
     if (ret)
     {
         Err_set(ret);
     }
 #endif
     return p;
+}
+
+void
+__RbTree_deallocNode(CRbNode* node, int32_t size)
+{
+#ifdef _MSC_VER
+    _aligned_free(node);
+#else
+    free(node);
+#endif
 }
