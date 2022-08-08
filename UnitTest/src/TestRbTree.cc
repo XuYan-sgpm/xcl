38de83c4c197a53695c66f45268ac3984b3961b1
BigDev <1404974345@qq.com>
2022-08-08 16:19:44 +0800

    add  rbtree test

--- UnitTest/src/TestRbTree.cc
+++ UnitTest/src/TestRbTree.cc
@@ -10,6 +10,19 @@ __cmpInt(void*, const void* lhs, const void* rhs)
     return *(int*)lhs - *(int*)rhs;
 }
 
+static bool
+__setInt(void*, void* lhs, const void* rhs)
+{
+    *(int*)lhs = *(int*)rhs;
+    return true;
+}
+
+static bool
+__destroy(void*, void*)
+{
+    return true;
+}
+
 TEST(RbTreeTest, func1)
 {
     CRbTree tree = RbTree_make(sizeof(int), Pool_def());
@@ -27,7 +40,6 @@ TEST(RbTreeTest, func1)
                                            &left);
         ASSERT_TRUE(_RbTree_addNode(&tree, left, par, node));
         ASSERT_TRUE(__RbTree_verify(&tree, __cmpInt, 0));
-        cout << "verify successfully" << endl;
     }
     CRbNode* iter = RbTree_begin(&tree);
     int prev = -1;
@@ -40,7 +52,38 @@ TEST(RbTreeTest, func1)
     {
         _RbTree_rmNode(&tree, RbTree_prev(&tree, iter));
         ASSERT_TRUE(__RbTree_verify(&tree, __cmpInt, 0));
-        cout << "verify successfully" << endl;
     }
     cout << endl;
+}
+
+TEST(RbTreeTest, func2)
+{
+    auto tree = RbTree_make(sizeof(int), Pool_def());
+    ASSERT_TRUE(RbTree_init(&tree));
+    for (int i = 0; i < 1024; i++)
+    {
+        int val = rand();
+        auto node = __RbTree_allocNode(&tree);
+        *(int*)node->attach = val;
+        bool left;
+        auto par = __RbTree_findAddPos(&tree,
+                                       false,
+                                       node->attach,
+                                       __cmpInt,
+                                       0,
+                                       &left);
+        ASSERT_TRUE(_RbTree_addNode(&tree, left, par, node));
+        ASSERT_TRUE(__RbTree_verify(&tree, __cmpInt, 0));
+    }
+    auto copy = RbTree_make(sizeof(int), Pool_def());
+    ASSERT_TRUE(RbTree_init(&copy));
+    ASSERT_TRUE(__RbTree_copy(&tree, __setInt, 0, &copy));
+    auto iter1 = RbTree_begin(&tree);
+    auto iter2 = RbTree_begin(&copy);
+    ASSERT_EQ(tree.count, copy.count);
+    for (; iter1 != RbTree_end(&tree);
+         iter1 = RbTree_next(&tree, iter1), iter2 = RbTree_next(&copy, iter2))
+    {
+        ASSERT_EQ(*(int*)iter1->attach, *(int*)iter2->attach);
+    }
 }
