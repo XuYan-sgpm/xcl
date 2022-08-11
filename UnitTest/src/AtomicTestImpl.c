884bef8da5444b917358138724c4f4d1235f791d
xuyan <1404974345@qq.com>
2022-08-11 13:37:57 +0800

    modify atomic tests

--- UnitTest/src/AtomicTestImpl.c
+++ UnitTest/src/AtomicTestImpl.c
@@ -7,8 +7,8 @@ __AtomicTest_func1()
 {
     ALIGNED(8) ATOMIC(int64_t) x;
     __Atomic_store64(&x, 89l, memory_order_release);
-    printf("%ld\n", __Atomic_load64(&x, memory_order_consume));
+    printf("%lld\n", __Atomic_load64(&x, memory_order_consume));
     int64_t val = 89;
     printf("%d\n", __Atomic_weakCas64(&x, &val, 876, memory_order_acq_rel));
-    printf("%ld\n", __Atomic_load64(&x, memory_order_consume));
+    printf("%lld\n", __Atomic_load64(&x, memory_order_consume));
 }
